f3842ff31a009a6e0ed7dcc9c20e9f086ac33711
Welton Rodrigo <rodrigo@familianascimento.org>
2012-04-19 13:56:21 -0400

    Adição de script para comparar saídas

---
+++ tradecompare.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE: tradecompare.pl
+#
+#        USAGE: ./tradecompare.pl  
+#
+#  DESCRIPTION: Mostra as diferenças entre dois resultados do trademax.
+#  				Muito útil para determinar se são os mesmos participantes.
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: WELTON RODRIGO TORRES NASCIMENTO (rodrigo@familianascimento.org), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 19-04-2012 13:20:13
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+use File::Temp;
+
+# Infiles
+open my $a, "<", shift or die "$0: Erro ao abrir o arquivo: $!";
+open my $b, "<", shift or die "$0: Erro ao abrir o arquivo: $!";
+
+# Outfiles
+my $a_out = new File::Temp();
+my $b_out = new File::Temp();
+
+my @a;
+my @b;
+
+while(<$a>){
+	s/\s+receives.*$// and push @a, $_;
+	last if m/^ITEM SUMMARY/;
+}
+
+while(<$b>){
+	s/\s+receives.*$// and push @b, $_;
+	last if m/^ITEM SUMMARY/;
+}
+
+print $a_out $_ foreach sort @a;
+print $b_out $_ foreach sort @b;
+
+close $a_out;
+close $b_out;
+
+system qw/diff --side-by-side/, $a_out->filename, $b_out->filename;
